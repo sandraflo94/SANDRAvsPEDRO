7a9385f9f1eb0f785dbef7c1c2d05d94fb226911
Sandra Florindo <sandraf.sf82@gmail.com>
2022-04-11 14:22:21 +0100

    ola pedro

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -2,6 +2,7 @@ public class main {
     public static void main(String[] args) {
         System.out.println("adeus");
         System.out.println("ola pedro");
+        System.out.println("que andaste a fazer?");
 
     }
 }
