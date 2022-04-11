9b75381fc2a23bd33d99b7ed4a3f0543854bf92e
Pedro Cubal <pedrocubal@gmail.com>
2022-04-11 14:21:46 +0100

    acrescentei coisas

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,7 +1,7 @@
 public class main {
     public static void main(String[] args) {
-        System.out.println("adeus");
-        System.out.println("ola pedro");
+        System.out.println("ola Sandra");
+        System.out.println("Quem matou o Diogo também matou o Flávio.");
 
     }
 }
